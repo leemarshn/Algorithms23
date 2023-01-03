c7988b6fcdbf9be1eab95b5bb61d033bf4106200
leen <leendegwa@gmail.com>
2023-01-03 21:53:42 +0300

    Brute force: two sum

---
+++ src/TwoSumArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class TwoSumArray {
+    public static void main(String[] args) {
+
+        int[] arr = {2,3,4,5,6,7};
+        int target = 5;
+
+        for (int k: getIndices(arr, target)) {
+            System.out.println(k);
+        }
+
+    }
+
+    static int[] getIndices(int[] array, int target){
+       for (int i=0; i<array.length; i++){
+           for (int k=i+1; k<array.length; k++){
+               if (array[i]+array[k]==target){
+                   return new int[] {i, k};
+               }
+           }
+       }
+
+       return new int [] {0};
+    }
+}
